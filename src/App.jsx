9dcc6b3c08d88fe10bc178cb59894fec2c2f90b4
NormanJaimes <normane_@hotmail.com>
2024-04-05 08:03:05 -0500

    Secre la lista de criptomonedas, se agregó axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,41 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 function App() {
-  return <h1>Holanda</h1>;
+  const [criptos, setCriptos] = useState([]);
+
+  const API_URL = import.meta.env.VITE_API_URL;
+
+  useEffect(() => {
+    // fetch(`${API_URL}assets`)
+    axios
+      .get(`${API_URL}assets`)
+      // .then((res) => res.json())
+      .then((data) => {
+        // setCriptos(data.data);
+        setCriptos(data.data.data);
+      })
+      .catch((err) => {
+        console.error('La petición falló ' + err.message);
+      });
+
+    return () => {};
+  }, []);
+
+  if (!criptos) return <h1>Cargando...</h1>;
+
+  return (
+    <>
+      <h1>Lista Criptomonedas</h1>
+      <ul>
+        {criptos.map(({ id, name, priceUsd }) => (
+          <li key={id}>
+            Nombre: {name} Precio: {priceUsd}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
 
 export default App;
